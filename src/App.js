bc9b2b2043712742bfb4fed4640d3c396b59bea3
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-01-06 18:40:04 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificationModal from './components/VerificationModal/VerificationModal';
+
+function App() {
+  return (
+    <div>
+      <VerificationModal />
+    </div>
+  );
+}
+
+export default App;
